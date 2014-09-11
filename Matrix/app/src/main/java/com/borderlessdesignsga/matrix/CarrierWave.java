3aacea760b63a2d578bfb2340b4cd7d1fa226ca3
Wesley Carlsen <wesley720@bellsouth.net>
2014-09-11 11:12:11 -0400

    Update CarrierWave.java
    Further implement code to send a single byte over audio (for now just getting some square waves on output), hope to test this on oscilloscope soon to make sure it looks correct.

--- Matrix/app/src/main/java/com/borderlessdesignsga/matrix/CarrierWave.java
+++ Matrix/app/src/main/java/com/borderlessdesignsga/matrix/CarrierWave.java
@@ -9,16 +9,36 @@ import android.media.AudioTrack;
  */
 public class CarrierWave {
     //Use AudioTrack to create left and right channel square waves at low frequency
+    //Single usage class, so it will be created, used, and destroyed per data burst sent form tablet/phone
     //clock = left channel (idle low)
     //data = right channel (idle low)
 
-    AudioTrack track;
+    private AudioTrack track;
 
     public CarrierWave() {
-        int sampleFrequency = 44100;
-        int bufferSize = AudioTrack.getMinBufferSize(sampleFrequency, AudioFormat.CHANNEL_OUT_STEREO,AudioFormat.ENCODING_PCM_16BIT);
-        track = new AudioTrack(AudioManager.STREAM_MUSIC, sampleFrequency, AudioFormat.CHANNEL_OUT_STEREO, AudioFormat.ENCODING_PCM_16BIT, bufferSize, AudioTrack.MODE_STATIC);
+        byte time = 1; //seconds
+        byte resolution = 2; //bytes per sample
+        int sampleRate = 44100; //samples per second
+        byte channels = 2; //unitless scalor
+        
+        int bufferSize = time * resolution * sampleRate * channels; //bytes
+        
+        track = new AudioTrack(AudioManager.STREAM_MUSIC, sampleRate, AudioFormat.CHANNEL_OUT_STEREO, AudioFormat.ENCODING_PCM_16BIT, bufferSize, AudioTrack.MODE_STATIC);
     }
 
-
+    public void send(byte data)
+    {
+        //byte goes between -128 and so we need to offset data to make it between 0 and 255
+        data += 128;
+        
+        //buffer to hold data
+        short buffer[] = new short[176400];
+        
+        //write data here
+        //possibly need to call reloadStaticData() method here
+        track.write(buffer, 0, buffer.length);
+        
+        //play track cause we're all done here!
+        track.play();
+    }
 }
